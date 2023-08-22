fa5f01cb2da95d8300498719febe56f6c734679f
kboss <atsugi.design@gmail.com>
2023-08-22 16:26:48 +0900

    map関数でデータを取得

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,40 @@
 import { collection, getDocs } from "firebase/firestore";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { db } from "../firebase";
 import "./Home.css";
 
 const Home = () => {
+  const [postList, setPostList] = useState([]);
+
   useEffect(() => {
     const getPosts = async () => {
       const data = await getDocs(collection(db, "posts"));
-      console.log(data);
-      console.log(data.docs);
-      console.log(data.docs.map((doc) => ({ doc })));
+      // console.log(data);
+      // console.log(data.docs);
+      // console.log(data.docs.map((doc) => ({ doc })));
       console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
     getPosts();
   }, []);
 
   return (
     <div className="homePage">
-      <div className="postContents">
-        <div className="postHeader">
-          <h1>タイトル</h1>
-        </div>
+      {postList.map((post) => {
+        return (
+          <div className="postContents" key={post.id}>
+            <div className="postHeader">
+              <h1>{post.title}</h1>
+            </div>
 
-        <div className="postTextContainer">
-          今はReactの学習中です。これから頑張ってReactエンジニアとして活躍していきます。
-        </div>
-        <div className="nameAndDeleteButton">
-          <h3>@Kakizaki</h3>
-          <button>削除</button>
-        </div>
-      </div>
+            <div className="postTextContainer">{post.postText}</div>
+            <div className="nameAndDeleteButton">
+              <h3>@{post.author.username}</h3>
+              <button>削除</button>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
